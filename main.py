4b908b90c7a33a2a5e99cc8549cd7821c5119ca2
Gabriel Tamujo <gabriel.meyrer@cwi.com.br>
2020-08-31 19:54:54 -0300

    fix: tf session

--- main.py
+++ main.py
@@ -7,35 +7,34 @@ logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
-sess = gpt2.start_tf_sess()
-gpt2.load_gpt2(sess,
-                model_name='model',
-                model_dir='')
+with gpt2.start_tf_sess() as sess:
 
-@app.route('/', methods=['GET'])
-def get(): return Response('', status=200, mimetype='application/json')
+    gpt2.load_gpt2(sess,
+                   model_name='model',
+                   model_dir='')
 
+    @app.route('/', methods=['GET'])
+    def get(): return Response('', status=200, mimetype='application/json')
 
-@app.route('/', methods=['POST'])
-def post():
-    input_text = request.get_json(force=True)['text']
-    if input_text == '':
-        abort(400, description="The input text cannot be null.")
+    @app.route('/', methods=['POST'])
+    def post():
+        input_text = request.get_json(force=True)['text']
+        if input_text == '':
+            abort(400, description="The input text cannot be null.")
 
-    result = gpt2.generate(sess,
-                           model_name='model',
-                           model_dir='',
-                           seed=99,
-                           nsamples=5,
-                           batch_size=5,
-                           length=8,
-                           temperature=0,
-                           top_k=10,
-                           top_p=.85,
-                           return_as_list=True)
-    app.logger.info(f"Returning list of predictions: {result}")
-    return Response(json.dumps({'result': result}), status=200, mimetype='application/json')
+        result = gpt2.generate(sess,
+                               model_name='model',
+                               model_dir='',
+                               seed=99,
+                               nsamples=5,
+                               batch_size=5,
+                               length=8,
+                               temperature=0,
+                               top_k=10,
+                               top_p=.85,
+                               return_as_list=True)
+        app.logger.info(f"Returning list of predictions: {result}")
+        return Response(json.dumps({'result': result}), status=200, mimetype='application/json')
 
-
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=3030, debug=True)
+    if __name__ == '__main__':
+        app.run('0.0.0.0', port=3030, debug=True)
