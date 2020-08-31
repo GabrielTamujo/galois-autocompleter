78575bb75eef31397cf65adf79fbd6e8f027619d
Gabriel Tamujo <gabriel.meyrer@cwi.com.br>
2020-08-31 17:21:57 -0300

    Test

--- main.py
+++ main.py
@@ -16,17 +16,19 @@ def post():
     if input_text == '':
         abort(400, description="The input text cannot be null.")
 
-    sess = gpt2.start_tf_sess()
-    gpt2.load_gpt2(sess, model_name='model')
-    result = gpt2.generate(sess,
-                           seed=99,
-                           nsamples=5,
-                           batch_size=5,
-                           length=8,
-                           temperature=0,
-                           top_k=10,
-                           top_p=.85,
-                           return_as_list=True)
+    result = ['1', '2']
+    
+    # sess = gpt2.start_tf_sess()
+    # gpt2.load_gpt2(sess, model_name='model')
+    # result = gpt2.generate(sess,
+    #                        seed=99,
+    #                        nsamples=5,
+    #                        batch_size=5,
+    #                        length=8,
+    #                        temperature=0,
+    #                        top_k=10,
+    #                        top_p=.85,
+    #                        return_as_list=True)
 
     return Response(json.dumps({'result': result}), status=200, mimetype='application/json')
 
