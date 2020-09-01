e3a2a8b5233d9167afcb9b3bbef415d2862e784b
Gabriel Tamujo <gabriel.meyrer@cwi.com.br>
2020-09-01 12:38:45 -0300

    Test

--- main.py
+++ main.py
@@ -7,48 +7,49 @@ import gpt_2_simple as gpt2
 
 app = Flask(__name__)
 
-# gpu_options = tf.GPUOptions(allow_growth=True)
-# config = tf.ConfigProto(intra_op_parallelism_threads=0,
-#                         inter_op_parallelism_threads=0,
-#                         allow_soft_placement=True,
-#                         gpu_options=gpu_options)
 
-app.logger.info('Starting new tensorflow session.')
-with gpt2.start_tf_sess() as sess:
+with tf.Graph().as_default():
+    gpu_options = tf.GPUOptions(allow_growth=True)
+    config = tf.ConfigProto(intra_op_parallelism_threads=0,
+                            inter_op_parallelism_threads=0,
+                            allow_soft_placement=True,
+                            gpu_options=gpu_options)
+    app.logger.info('Starting new tensorflow session.')
 
-    app.logger.info('Starting to load GPT-2 model.')
-    gpt2.load_gpt2(sess,
-                   model_name='model',
-                   model_dir='',
-                   multi_gpu=True)
+    with tf.Session(config=config) as sess:
+        app.logger.info('Starting to load GPT-2 model.')
+        gpt2.load_gpt2(sess,
+                       model_name='model',
+                       model_dir='',
+                       multi_gpu=True)
 
-    class Autocomplete(Resource):
+        class Autocomplete(Resource):
 
-        def get(self): return ''
+            def get(self): return ''
 
-        def post(self):
-            input_text = request.get_json(force=True)['text']
-            if input_text == '':
-                abort(400, description="The input text cannot be null.")
+            def post(self):
+                input_text = request.get_json(force=True)['text']
+                if input_text == '':
+                    abort(400, description="The input text cannot be null.")
 
-            app.logger.info('Starting to generate samples')
-            result = gpt2.generate(sess,
-                                   model_name='model',
-                                   model_dir='',
-                                   prefix=input_text,
-                                   include_prefix=False,
-                                   nsamples=5,
-                                   batch_size=5,
-                                   length=8,
-                                   temperature=0.5,
-                                   return_as_list=True)
+                app.logger.info('Starting to generate samples')
+                result = gpt2.generate(sess,
+                                       model_name='model',
+                                       model_dir='',
+                                       prefix=input_text,
+                                       include_prefix=False,
+                                       nsamples=5,
+                                       batch_size=5,
+                                       length=8,
+                                       temperature=0.5,
+                                       return_as_list=True)
 
-            app.logger.info(f"Returning list of predictions: {result}")
-            return Response(json.dumps({'result': result}), status=200, mimetype='application/json')
+                app.logger.info(f"Returning list of predictions: {result}")
+                return Response(json.dumps({'result': result}), status=200, mimetype='application/json')
 
-    app = Flask(__name__)
-    api = Api(app)
-    api.add_resource(Autocomplete, '/autocomplete')
+        app = Flask(__name__)
+        api = Api(app)
+        api.add_resource(Autocomplete, '/autocomplete')
 
-    if __name__ == '__main__':
-        app.run('0.0.0.0', port=3030, debug=True)
+        if __name__ == '__main__':
+            app.run('0.0.0.0', port=3030, debug=True)
