29b932dbfa0a341ef076588b559333d9a736e252
Gabriel Tamujo <gabriel.meyrer@cwi.com.br>
2020-09-01 10:18:53 -0300

    Test

--- main.py
+++ main.py
@@ -6,38 +6,45 @@ import gpt_2_simple as gpt2
 
 app = Flask(__name__)
 
-with gpt2.start_tf_sess() as sess:
+# with gpt2.start_tf_sess() as sess:
 
-    gpt2.load_gpt2(sess,
-                   model_name='model',
-                   model_dir='')
+#     gpt2.load_gpt2(sess,
+#                    model_name='model',
+#                    model_dir='')
 
-    class Autocomplete(Resource):
+class Autocomplete(Resource):
 
-        def get(self): return ''
+    def get(self): return ''
 
-        def post(self):
-            input_text = request.get_json(force=True)['text']
-            if input_text == '':
-                abort(400, description="The input text cannot be null.")
+    def post(self):
+        input_text = request.get_json(force=True)['text']
+        if input_text == '':
+            abort(400, description="The input text cannot be null.")
 
-            result = gpt2.generate(sess,
-                                   model_name='model',
-                                   model_dir='',
-                                   seed=99,
-                                   nsamples=5,
-                                   batch_size=5,
-                                   length=8,
-                                   temperature=0,
-                                   top_k=10,
-                                   top_p=.85,
-                                   return_as_list=True)
-            app.logger.info(f"Returning list of predictions: {result}")
-            return Response(json.dumps({'result': result}), status=200, mimetype='application/json')
+        sess = gpt2.start_tf_sess()
+        
+        gpt2.load_gpt2(sess,
+                model_name='model',
+                model_dir='')
+        
+        result = gpt2.generate(sess,
+                                model_name='model',
+                                model_dir='',
+                                prefix=input_text,
+                                seed=99,
+                                nsamples=5,
+                                batch_size=5,
+                                length=8,
+                                temperature=0,
+                                top_k=10,
+                                top_p=.85,
+                                return_as_list=True)
+        app.logger.info(f"Returning list of predictions: {result}")
+        return Response(json.dumps({'result': result}), status=200, mimetype='application/json')
 
-    app = Flask(__name__)
-    api = Api(app)
-    api.add_resource(Autocomplete, '/autocomplete')
+app = Flask(__name__)
+api = Api(app)
+api.add_resource(Autocomplete, '/autocomplete')
 
-    if __name__ == '__main__':
-        app.run('0.0.0.0', port=3030, debug=True)
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=3030, debug=True)
