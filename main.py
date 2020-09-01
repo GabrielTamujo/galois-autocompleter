1ef753ae43f5dd8a359e8828b3c034a35d6189a6
Gabriel Tamujo <gabriel.meyrer@cwi.com.br>
2020-09-01 10:31:32 -0300

    Test

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ app = Flask(__name__)
 #                    model_name='model',
 #                    model_dir='')
 
+
 class Autocomplete(Resource):
 
     def get(self): return ''
@@ -22,25 +23,25 @@ class Autocomplete(Resource):
             abort(400, description="The input text cannot be null.")
 
         sess = gpt2.start_tf_sess()
-        
+
         gpt2.load_gpt2(sess,
-                model_name='model',
-                model_dir='')
-        
+                       model_name='model',
+                       model_dir='')
+
         result = gpt2.generate(sess,
-                                model_name='model',
-                                model_dir='',
-                                prefix=input_text,
-                                seed=99,
-                                nsamples=5,
-                                batch_size=5,
-                                length=8,
-                                temperature=0,
-                                top_k=10,
-                                top_p=.85,
-                                return_as_list=True)
+                               model_name='model',
+                               model_dir='',
+                               prefix=input_text,
+                               nsamples=5,
+                               batch_size=5,
+                               length=8,
+                               temperature=0.5,
+                            #    top_k=10,
+                            #    top_p=.85
+                               )
         app.logger.info(f"Returning list of predictions: {result}")
         return Response(json.dumps({'result': result}), status=200, mimetype='application/json')
+
 
 app = Flask(__name__)
 api = Api(app)
