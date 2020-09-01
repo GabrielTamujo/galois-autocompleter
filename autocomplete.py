ced1dd25822ba2b1ab48d54daafd7edc42a0df53
Gabriel Tamujo <gabriel.meyrer@cwi.com.br>
2020-09-01 17:30:52 -0300

    Agora vai

--- autocomplete.py
+++ autocomplete.py
@@ -17,20 +17,23 @@ class Autocomplete():
                                model_dir='',
                                multi_gpu=True)
 
-    def predict(self, input_text):
+    def predict(self, request_body):
         with self.sess.as_default() as sess:
             with self.graph.as_default():
                 return gpt2.generate(sess,
                                      model_name='model',
                                      model_dir='',
-                                     seed=99,
-                                     prefix=input_text,
+                                     seed=request_body.get('seed', 99),
+                                     prefix="<|startoftext|>" +
+                                     request_body.get('text', ''),
                                      include_prefix=False,
                                      truncate="<|endoftext|>",
-                                     nsamples=5,
-                                     batch_size=5,
-                                     length=8,
-                                     top_k=10,
-                                     top_p=.85,
-                                     temperature=0,
+                                     nsamples=request_body.get('nsamples', 5),
+                                     batch_size=request_body.get(
+                                         'batch_size', 5),
+                                     length=request_body.get('length', 8),
+                                     top_k=request_body.get('top_k', 10),
+                                     top_p=request_body.get('top_p', .85),
+                                     temperature=request_body.get(
+                                         'temperature', 0.1),
                                      return_as_list=True)
--- main.py
+++ main.py
@@ -1,44 +1,23 @@
 from flask import Flask, request, Response, abort
 import json
-import gpt_2_simple as gpt2
+from autocomplete import Autocomplete
 import logging
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
-app.logger.info('Starting to create tensorflow session.')
-sess = gpt2.start_tf_sess(threads=1)
-
-app.logger.info('Starting to load the GPT-2 model.')
-gpt2.load_gpt2(sess,
-               model_name='model',
-               model_dir='',
-               multi_gpu=True)
+autocomplete = Autocomplete()
 
 
 @app.route('/autocomplete', methods=['GET'])
 def get(): return Response('', status=200)
 
 
-@app.route('/autocomplete', methods=['POST'])
+@app.route('/autocomplete', methods=['GET', 'POST'])
 def post():
-    request_body = request.get_json(force=True)
 
     app.logger.info('Starting to predict.')
-    result = gpt2.generate(sess,
-                           model_name='model',
-                           model_dir='',
-                           seed=request_body.get('seed', 99),
-                           prefix="<|startoftext|>" + request_body.get('text', ''),
-                           include_prefix=False,
-                           truncate="<|endoftext|>",
-                           nsamples=request_body.get('nsamples', 5),
-                           batch_size=request_body.get('batch_size', 5),
-                           length=request_body.get('length', 8),
-                           top_k=request_body.get('top_k', 10),
-                           top_p=request_body.get('top_p', .85),
-                           temperature=request_body.get('temperature', 0.1),
-                           return_as_list=True)
+    result = autocomplete.predict(request.get_json(force=True))
 
     app.logger.info(f"Returning list of predictions: {result}")
     return Response(json.dumps({'result': result}), status=200, mimetype='application/json')
