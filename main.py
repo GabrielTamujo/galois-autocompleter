322e7f746084aa54f5a34c7742e2e4b33d18656a
Gabriel Tamujo <gabriel.meyrer@cwi.com.br>
2020-09-01 10:53:08 -0300

    Update

--- main.py
+++ main.py
@@ -6,38 +6,33 @@ import gpt_2_simple as gpt2
 
 app = Flask(__name__)
 
-# with gpt2.start_tf_sess() as sess:
+with gpt2.start_tf_sess() as sess:
 
-#     gpt2.load_gpt2(sess,
-#                    model_name='model',
-#                    model_dir='')
+    class Autocomplete(Resource):
 
-class Autocomplete(Resource):
+        def get(self): return ''
 
-    def get(self): return ''
+        def post(self):
+            input_text = request.get_json(force=True)['text']
+            if input_text == '':
+                abort(400, description="The input text cannot be null.")
 
-    def post(self):
-        input_text = request.get_json(force=True)['text']
-        if input_text == '':
-            abort(400, description="The input text cannot be null.")
+            gpt2.load_gpt2(sess,
+                           model_name='model',
+                           model_dir='')
 
-        sess = gpt2.start_tf_sess()
-
-        gpt2.load_gpt2(sess,
-                model_name='model',
-                model_dir='')
-
-        result = gpt2.generate(sess,
-                                model_name='model',
-                                model_dir='',
-                                prefix=input_text,
-                                nsamples=5,
-                                batch_size=5,
-                                length=8,
-                                temperature=0.5,
-                                return_as_list=True)
-        app.logger.info(f"Returning list of predictions: {result}")
-        return Response(json.dumps({'result': result}), status=200, mimetype='application/json')
+            result = gpt2.generate(sess,
+                                   model_name='model',
+                                   model_dir='',
+                                   prefix=input_text,
+                                   nsamples=5,
+                                   batch_size=5,
+                                   length=8,
+                                   temperature=0.5,
+                                   return_as_list=True)
+                                   
+            app.logger.info(f"Returning list of predictions: {result}")
+            return Response(json.dumps({'result': result}), status=200, mimetype='application/json')
 
 app = Flask(__name__)
 api = Api(app)
