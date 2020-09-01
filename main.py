ec0c8fd4f2c4d1b2aa07972bb798afcb1657e530
Gabriel Tamujo <gabriel.meyrer@cwi.com.br>
2020-09-01 10:37:49 -0300

    Update

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ app = Flask(__name__)
 #                    model_name='model',
 #                    model_dir='')
 
-
 class Autocomplete(Resource):
 
     def get(self): return ''
@@ -25,23 +24,20 @@ class Autocomplete(Resource):
         sess = gpt2.start_tf_sess()
 
         gpt2.load_gpt2(sess,
-                       model_name='model',
-                       model_dir='')
+                model_name='model',
+                model_dir='')
 
         result = gpt2.generate(sess,
-                               model_name='model',
-                               model_dir='',
-                               prefix=input_text,
-                               nsamples=5,
-                               batch_size=5,
-                               length=8,
-                               temperature=0.5,
-                            #    top_k=10,
-                            #    top_p=.85
-                               )
+                                model_name='model',
+                                model_dir='',
+                                prefix=input_text,
+                                nsamples=5,
+                                batch_size=5,
+                                length=8,
+                                temperature=0.5,
+                                return_as_list=True)
         app.logger.info(f"Returning list of predictions: {result}")
         return Response(json.dumps({'result': result}), status=200, mimetype='application/json')
-
 
 app = Flask(__name__)
 api = Api(app)
