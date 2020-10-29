e13360162c46e012ab7d29261b05503af1eea131
Gabriel Tamujo Meyrer <gabriel.meyrer@cwi.com.br>
2020-10-29 11:32:07 -0300

    test

--- main.py
+++ main.py
@@ -147,7 +147,7 @@ def interact_model(model_name='model',
             return Response(json.dumps(response), status=200)
 
         if __name__ == '__main__':
-            app.run('0.0.0.0', port=3030, debug=True, threaded=True)
+            app.run('0.0.0.0', port=3030, debug=True)
 
 
 interact_model()
