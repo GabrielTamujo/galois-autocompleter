f7678fe8be818a81745f9f5000199956b2e601d0
Gabriel Tamujo Meyrer <gabriel.meyrer@cwi.com.br>
2020-11-09 09:32:31 -0300

    fix: prediction process

--- prediction_utils.py
+++ prediction_utils.py
@@ -3,7 +3,6 @@ import re
 def process(prediction):
     prediction = filter_noise(prediction)
     prediction = remove_new_line(prediction)
-    prediction = escape_spaces_from_beggining(prediction)
     return prediction
 
 def is_valid_prediction(prediction):
@@ -14,12 +13,6 @@ def filter_noise(prediction):
 
 def remove_new_line(prediction):
     return prediction.split('\n')[0]
-
-def escape_spaces_from_beggining(prediction):
-    matrix = prediction.split(' ')
-    while matrix !=[] and not matrix[0]:
-        del matrix[0]
-    return ' '.join(matrix)
 
 def get_first_token(prediction):
     delimiters = [' ',
