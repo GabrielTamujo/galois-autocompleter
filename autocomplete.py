ff2a4e934ef3b5dd40fcce02143cf7550cb363cb
Gabriel Tamujo Meyrer <gabriel.meyrer@cwi.com.br>
2020-11-13 14:32:17 -0300

    feat: created custom start tf sess

--- autocomplete.py
+++ autocomplete.py
@@ -5,10 +5,18 @@ import tensorflow as tf
 class Autocomplete():
 
     def __init__(self):
-        self.sess = gpt2.start_tf_sess()
+        self.sess = start_tf_sess()
         self.graph = tf.get_default_graph()
         self.load_gpt2()
 
+    def start_tf_sess(self):
+        gpu_options = tf.compat.v1.GPUOptions(allow_growth=True)
+        config = tf.compat.v1.ConfigProto(intra_op_parallelism_threads=0, 
+                        inter_op_parallelism_threads=0,
+                        allow_soft_placement=True, 
+                        gpu_options=gpu_options)
+        return tf.compat.v1.Session(config=config)
+    
     def load_gpt2(self):
         with self.sess.as_default() as sess:
             with self.graph.as_default():
