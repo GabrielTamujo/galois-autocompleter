0e1f87dad36fe139427cfedfb260ba60785fb6b9
Gabriel Tamujo Meyrer <gabriel.meyrer@cwi.com.br>
2020-12-04 19:11:00 -0300

    feat: created control database

--- predictor.py
+++ predictor.py
@@ -2,6 +2,7 @@ import torch
 from config import Config
 from transformers import GPT2TokenizerFast, GPT2LMHeadModel
 from suggestions import create_suggestions
+from control_database import ControlDatabase
 import subprocess
 import json
 
