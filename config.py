09142aa71732cdb3ddd24ad8cffe7b19d57f4fd5
Gabriel Tamujo Meyrer <gabriel.meyrer@cwi.com.br>
2020-12-16 11:37:33 -0300

    Update

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ class Config:
     def __init__(self, config, model_max_length):
         self.NUM_TOKENS_TO_PREDICT = config.get("num_tokens_to_predict", 16)
         self.MAX_INPUT_TOKENS_LENGTH = model_max_length - self.NUM_TOKENS_TO_PREDICT
-        self.MAX_INPUT_TEXT_LENGTH = config.get("max_input_text_length", 1000)
+        self.MAX_INPUT_TEXT_LENGTH = config.get("max_input_text_length", 3000)
         self.NUM_RETURN_SEQUENCES = config.get("num_return_sequences", 2)
         self.TOP_K = config.get("top_k", 50)
         self.TOP_P = config.get("top_p", 0.85)
